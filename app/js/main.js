cb25102a152c191266d9b2d68d5b198f2e666eb3
first commit <stebataras@gmail.com>
2023-05-19 12:15:33 +0200

    drop-down menu completed!

--- app/js/main.js
+++ app/js/main.js
@@ -1,11 +1,45 @@
 const hamburger = document.querySelector('.hamburger'),
       menu = document.querySelector('.menu'),
-      close = document.querySelector('.menu__close');
+      close = document.querySelector('.menu__close'),
+
+      menulist = document.querySelector('.menu__list'), //test menu list
+
+      menulink1 = document.querySelector('.first'), //test menu link
+      menulink2 = document.querySelector('.second'),
+      menulink3 = document.querySelector('.third'),
+      menulink4 = document.querySelector('.fourth'),
+      menulink5 = document.querySelector('.fifth'),
+      menulink6 = document.querySelector('.sixth');
+
+
+
 
 hamburger.addEventListener('click', () => {    //add class "active"
-    menu.classList.add('active');
+    menu.classList.add('active');   
 });
 
 close.addEventListener('click', () => {        //remove class "active"
     menu.classList.remove('active');
+});
+
+
+
+hamburger.addEventListener('click', () => {        //test menu list
+    menulist.classList.add('active');
+    menulink1.classList.add('active');
+    menulink2.classList.add('active');
+    menulink3.classList.add('active');
+    menulink4.classList.add('active');
+    menulink5.classList.add('active');
+    menulink6.classList.add('active');
+});
+
+close.addEventListener('click', () => {        //test menu list
+    menulist.classList.remove('active');
+    menulink1.classList.remove('active');
+    menulink2.classList.remove('active');
+    menulink3.classList.remove('active');
+    menulink4.classList.remove('active');
+    menulink5.classList.remove('active');
+    menulink6.classList.remove('active');
 });
